4cff156f3cc5d3401eb5f2491ad8a795e332f5cb
javis250 <pacoleon400@gmail.com>
2020-06-11 14:37:45 -0500

    proyecto prueba

---
+++ keys.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+
+    database: {
+        host: 'sql3.freemysqlhosting.net',
+        user: 'sql3347090',
+        password: 'qGKEnWEKDt',
+        database: 'sql3347090'
+    }
+
+};
